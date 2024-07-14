e8c7d5d9338e83374fc05ae6222687b7d8c2056a
Prateekm809 <pm6837759@gmail.com>
2024-07-15 01:33:39 +0530

    backend completed

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ const { title } = require("process");
 require('dotenv').config();
 
 mongoose.connect(process.env.mongo_uri)
-const todoSchema = mongoose.schema({
-    title: string,
-    description: string,
-    completed: boolean
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
 })
 
 const todo = mongoose.model('todos',todoSchema);
