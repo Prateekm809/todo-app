cfa2c029f38a97fce25dd1dd578e515c47a3a00a
Prateekm809 <pm6837759@gmail.com>
2024-07-15 00:07:15 +0530

    added zod authentication

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require("express")
 const app = express();
-
+const { createTodo } = require("./types")
+const { updateTodo } = require("./types")
 app.use(express.json);
 
 
 
 app.post("/todo", (req,res)=>{
-
+const createBody = req.body;
+const parseBody = createTodo.safeParse(createBody);
+if (!parseBody.success){
+    res.status(411).json({
+        msg:"Your sent the wrong inputs",
+    })
+    return;
+}
 })
 
 app.get("/todos",(req, res)=>{
-
+    
 })
 
 app.put("/completed",(req,res)=>{
-
+    const updateBody = req.body;
+    const parseBody = updateTodo.safeParse(updateBody);
+    if(!parseBody.success){
+        res.status(411).json({
+            msg:"Your sent the wrong inputs",
+        })
+       return;
+    }
+    return
 })
